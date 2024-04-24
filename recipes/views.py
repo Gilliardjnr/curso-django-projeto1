073b2b25034d7fb69cd715ea3b58eb4058e29026
Gilliard Junior <gilliardtorresjuni@hotmail.com>
2024-04-23 21:12:44 -0300

    ...

--- recipes/views.py
+++ recipes/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
-# Create your views here.
+
+def home(request):
+    return HttpResponse('Você está na página inicial')
+
+
+def sobre(request):
+    return HttpResponse('Você está na sessão sobre')
+
+
+def contato(request):
+    return HttpResponse('Você está na sessão contatos')
